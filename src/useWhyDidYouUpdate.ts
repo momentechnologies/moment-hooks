82f9f5a54923c4401e96bebca49552efcf3b9484
Max Moeschinger <m.moeschinger@gmail.com>
2020-01-14 11:02:32 +0100

    remove tslint rule

--- src/useWhyDidYouUpdate.ts
+++ src/useWhyDidYouUpdate.ts
@@ -24,13 +24,9 @@ export default (name: string, props: IProps) => {
             });
 
             if (Object.keys(after).length) {
-                // tslint:disable-next-line:no-console
                 console.group(`[wdyu]: ${name}`);
-                // tslint:disable-next-line:no-console
                 console.log('Before:', before);
-                // tslint:disable-next-line:no-console
                 console.log('After:', after);
-                // tslint:disable-next-line:no-console
                 console.groupEnd();
             }
         }
